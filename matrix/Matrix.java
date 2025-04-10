4cc2e6efc35900db29173cdbbad071845b0dc941
Joeyb555 <josephboucher5@gmail.com>
2025-04-10 10:27:43 -0400

    Move method main from Matrix.java → RunMatrix.java

--- matrix/Matrix.java
+++ matrix/Matrix.java
@@ -51,23 +51,4 @@ public class Matrix {
       }
     }
   }
-
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-
-    System.out.print("Enter the size of the matrix: ");
-    int size = sc.nextInt();
-
-    Matrix matrix = new Matrix(size);
-
-    matrix.populateMatrix();
-    System.out.println("Original Matrix:");
-    matrix.printMatrix();
-
-    matrix.flipMatrix();
-    System.out.println("\nFlipped Matrix:");
-    matrix.printMatrix();
-
-    sc.close();
-  }
 }
--- matrix/RunMatrix.java
+++ matrix/RunMatrix.java
@@ -2,5 +2,21 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter the size of the matrix: ");
+    int size = sc.nextInt();
+
+    Matrix matrix = new Matrix(size);
+
+    matrix.populateMatrix();
+    System.out.println("Original Matrix:");
+    matrix.printMatrix();
+
+    matrix.flipMatrix();
+    System.out.println("\nFlipped Matrix:");
+    matrix.printMatrix();
+
+    sc.close();
   }
 }
