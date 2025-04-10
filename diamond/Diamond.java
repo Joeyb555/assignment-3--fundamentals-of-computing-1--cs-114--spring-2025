c6e9644944b42d2014d697e0c9c47960dce2f3ab
Joeyb555 <josephboucher5@gmail.com>
2025-04-10 09:39:13 -0400

    04-10-2025

--- diamond/Diamond.java
+++ diamond/Diamond.java
@@ -1,4 +1,40 @@
+import java.util.Scanner;
+
 public class Diamond {
+  private static Scanner scanner = new Scanner(System.in);
+  private static int input, middle;
+
   public static void main(String[] args) {
+    System.out.print("Enter a number:");
+    input = scanner.nextInt();
+
+    if (input % 2 == 0)
+      System.out.println();
+
+    middle = input / 2;
+
+  for (int i = 0; i <= middle; i++) {
+    for (int j = 0; j < middle - i; j++) {
+      System.out.print(" ");
+    }
+
+    for (int j = 0; j < 2 * i + 1; j++) {
+      System.out.print("*");
+    }
+
+    System.out.println();
   }
+
+  for (int i = middle - 1; i >= 0; i--) {
+    for (int j = 0; j < middle - i; j++) {
+      System.out.print(" ");
+    }
+
+    for (int j = 0; j < 2 * i + 1; j++) {
+      System.out.print("*");
+    }
+
+    System.out.println();
+  }
+ }
 }
