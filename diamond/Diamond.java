4a6b647a1844b6954cc7b5154a1297456bab33b8
Joeyb555 <josephboucher5@gmail.com>
2025-04-10 08:53:48 -0400

    Update Diamond.java

--- diamond/Diamond.java
+++ diamond/Diamond.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 
 public class Diamond {
   private static Scanner scanner = new Scanner(System.in);
-  private static int n, mid;
+  private static int input, middle;
 
   public static void main(String[] args) {
-    System.out.print("Enter an integer for the diamond size:");
-    n = scanner.nextInt();
+    System.out.print("Enter a number:");
+    input = scanner.nextInt();
 
-    if (n % 2 == 0)
+    if (input % 2 == 0)
       System.out.println();
 
-    mid = n / 2;
+    middle = input / 2;
 
-  for (int i = 0; i <= mid; i++) {
-    for (int j = 0; j < mid - i; j++) {
+  for (int i = 0; i <= middle; i++) {
+    for (int j = 0; j < middle - i; j++) {
       System.out.print(" ");
     }
 
@@ -25,8 +25,8 @@ public class Diamond {
     System.out.println();
   }
 
-  for (int i = mid - 1; i >= 0; i--) {
-    for (int j = 0; j < mid - i; j++) {
+  for (int i = middle - 1; i >= 0; i--) {
+    for (int j = 0; j < middle - i; j++) {
       System.out.print(" ");
     }
 
@@ -36,29 +36,5 @@ public class Diamond {
 
     System.out.println();
   }
-}
-
-public static Scanner getScanner() {
-  return scanner;
-}
-
-public static void setScanner(Scanner scanner) {
-  Diamond.scanner = scanner;
-}
-
-public static int getN() {
-  return n;
-}
-
-public static void setN(int n) {
-  Diamond.n = n;
-}
-
-public static int getMid() {
-  return mid;
-}
-
-public static void setMid(int mid) {
-  Diamond.mid = mid;
-  }
+ }
 }
